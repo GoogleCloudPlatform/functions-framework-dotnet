0f8f985032fa14d5857cd0c7f57705858a6d2b57
Jon Skeet <jonskeet@google.com>
2020-03-03 02:50:48 -0500

    Initial prototype of Functions Framework for .NET
    The instructions in the README.md won't work until we've published the
    package to nuget.org, but the code in this repository is ready to make
    that happen.
    
    The three projects are:
    
    - Google.Cloud.Functions.Framework: the core framework that libraries
      will all depend on. (Currently just the IHttpFunction interface.)
    - Google.Cloud.Functions.Invoker: the code responsible for setting up
      the web server to serve the given function.
    - Google.Cloud.Functions.Templates: templates to allow "dotnet new"
      to create simple projects (initially just HTTP, but more templates
      can be added easily.)
    
    Change-Id: I3052fb09d5a3e6b5ad61e3c87bb15b571b8f8367

---
+++ Google.Cloud.Functions.Invoker/EntryPoint.cs
@@ -0,0 +1,80 @@
+﻿// Copyright 2020, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Google.Cloud.Functions.Invoker
+{
+    public static class EntryPoint
+    {
+        public static async Task<int> StartAsync(Assembly functionAssembly, string[] args)
+        {            
+            RequestDelegate handler = BuildHandler(functionAssembly, args);
+            int port = DeterminePort(args);
+            var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .ConfigureKestrel(serverOptions => serverOptions.Listen(IPAddress.Any, port))
+                    .Configure(app => app.Run(handler))
+                );
+            await builder.Build().RunAsync();
+            return 0;
+        }
+
+        private static RequestDelegate BuildHandler(Assembly functionAssembly, string[] args)
+        {
+            // TODO: Better command line parsing, e.g. --target=xyz --port=5000
+            var target = args.FirstOrDefault() ?? Environment.GetEnvironmentVariable("FUNCTION_NAME");
+            if (string.IsNullOrEmpty(target))
+            {
+                throw new Exception("No target provided");
+            }
+            var type = functionAssembly.GetType(target);
+            if (type is null)
+            {
+                throw new Exception($"Can't load target type '{target}'");
+            }
+            var instance = Activator.CreateInstance(type);
+            return instance switch
+            {
+                IHttpFunction function => context => function.HandleAsync(context),
+                _ => throw new Exception("Function doesn't support known interfaces")
+            };
+        }
+
+        private static int DeterminePort(string[] args)
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable("PORT");
+            if (!string.IsNullOrEmpty(environmentVariable))
+            {
+                if (!int.TryParse(environmentVariable, NumberStyles.None, CultureInfo.InvariantCulture, out var parsed))
+                {
+                    throw new Exception($"Can't part PORT environment variable '{environmentVariable}'");
+                }
+                return parsed;
+            }
+            // TODO: Parse args
+            return 8080;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,46 +1,56 @@
 # Functions Framework for .NET
 
-An open source framework for writing lightweight, portable C# functions that
-run in a serverless environment. Functions written to this Framework will run
-in many different environments, including:
+An open source FaaS (Function as a service) framework for writing portable
+Java functions -- brought to you by the Google Cloud Functions team.
 
-*  [Cloud Run or Cloud Run for Anthos](https://cloud.google.com/run)
-*  Any other [Knative](https://github.com/knative)-based environment
-*  Your local development machine
+The Functions Framework lets you write lightweight functions that run in many
+different environments, including:
 
-The framework allows you to go from:
-
-```
-...
-```
-
-To:
-
-```sh
-curl http://my-url
-# Output: Hello, world!
-```
-
-All without needing to worry about writing an HTTP server or complicated
-request handling logic.
-
-For more information about the Functions Framework, see
-https://github.com/GoogleCloudPlatform/functions-framework
-
-## Features
-
-* 1
-* 2
-* 3
+*   [Google Cloud Functions](https://cloud.google.com/functions/)
+*   Your local development machine
+*   [Cloud Run and Cloud Run on GKE](https://cloud.google.com/run/)
+*   [Knative](https://github.com/knative/)-based environments
 
 ## Installation
 
-Install the Functions Framework via the `dotnet` CLI:
+The Functions Framework for .NET requires the [.NET Core SDK 3.1](https://dotnet.microsoft.com/download).
 
+## Quickstart: Hello, World on your local machine
+
+First, install the template package into the .NET tooling:
+
+```sh
+$ dotnet new -i Google.Cloud.Functions.Templates::1.0.0-alpha01
 ```
-...
+
+Next, create a directory for your project, and use `dotnet new` to
+create a new HTTP function:
+
+```sh
+$ mkdir HelloFunctions
+$ cd HelloFunctions
+$ dotnet new gcf-http
 ```
 
-## Quickstart
+That will create `HelloFunctions.csproj` and `Function.cs` in
+the current directory. Edit `Function.cs` to have a look at what's
+required, and provide a custom message if you want.
 
-...
+## Run the function
+
+The Functions Framework needs to know the target function to run.
+This is the name of the class implementing `IHttpFunction` (and other interfaces later).
+There are two ways of specifying this:
+
+- Using the `FUNCTION_NAME` environment variable
+- Passing it on the command line as the first argument to the program
+
+```sh
+$ dotnet run HelloFunctions.Function
+```
+
+The default port is 8080; this can be changed with the `PORT`
+environment variable.
+
+Once the server is running, browse to http://localhost:8080 to
+invoke the function. Press Ctrl-C in the console to stop the server.
