a31c034525c90691a6711a795826c7df4ef7aef2
Jon Skeet <jonskeet@google.com>
2020-04-02 08:10:31 +0100

    Disable packing warning in Google.Cloud.Functions.SampleEventFunction

--- Google.Cloud.Functions.SampleEventFunction/generate.sh
+++ Google.Cloud.Functions.SampleEventFunction/generate.sh
@@ -15,3 +15,6 @@ dotnet add reference ../Google.Cloud.Functions.Invoker
 # Explicitly import the MSBuild targets (not necessary for NuGet packages)
 sed -i '2 i \ \ <Import Project="../Google.Cloud.Functions.Invoker/targets/Google.Cloud.Functions.Invoker.targets"/>' *.csproj
 sed -i '3 i \ \ <Import Project="../Google.Cloud.Functions.Invoker/targets/Google.Cloud.Functions.Invoker.props"/>' *.csproj
+
+# Disable packing warning, to keep Travis warning-free
+sed -i 's/<PropertyGroup>/<PropertyGroup>\n    <WarnOnPackingNonPackableProject>False<\/WarnOnPackingNonPackableProject>/g' *.csproj
