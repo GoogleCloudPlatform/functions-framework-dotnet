345131dda69bd3df8614a95c9c4c51d683e6c85a
Jon Skeet <jonskeet@google.com>
2020-04-02 08:07:13 +0100

    Update templates
    - Add the event function template
    - Update HTTP function classifications and group identity now I understand them better
    - Update everything to the upcoming 1.0.0-alpha02 version

---
+++ Google.Cloud.Functions.Templates/templates/event-function/Function.cs
@@ -0,0 +1,48 @@
+// Copyright 2020, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CloudNative.CloudEvents;
+using Google.Cloud.Functions.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace MyFunction
+{
+    public class Function : ICloudEventFunction
+    {
+        /// <summary>
+        /// Logic for your function goes here. Note that a Cloud Event function just consumes an event;
+        /// it doesn't provide any response.
+        /// </summary>
+        /// <param name="cloudEvent">The Cloud Event your function should respond to.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public Task HandleAsync(CloudEvent cloudEvent)
+        {
+            Console.WriteLine("Cloud event information:");
+            Console.WriteLine($"ID: {cloudEvent.Id}");
+            Console.WriteLine($"Source: {cloudEvent.Source}");
+            Console.WriteLine($"Type: {cloudEvent.Type}");
+            Console.WriteLine($"Subject: {cloudEvent.Subject}");
+            Console.WriteLine($"DataSchema: {cloudEvent.DataSchema}");
+            Console.WriteLine($"DataContentType: {cloudEvent.DataContentType}");
+            Console.WriteLine($"Time: {cloudEvent.Time?.ToUniversalTime():yyyy-MM-dd'T'HH:mm:ss.fff'Z'}");
+            Console.WriteLine($"SpecVersion: {cloudEvent.SpecVersion}");
+            Console.WriteLine($"Data: {cloudEvent.Data}");
+
+            // In this example, we don't need to perform any asynchronous operations, so the
+            // method doesn't need to be declared async.
+            return Task.CompletedTask;
+        }
+    }
+}
