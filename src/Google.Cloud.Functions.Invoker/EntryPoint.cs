9ca63d44a0c315f1652cf7593e2abcdfa0cc8109
Jon Skeet <jonskeet@google.com>
2020-04-07 08:10:42 +0100

    Remove unnecessary using directives

--- src/Google.Cloud.Functions.Invoker/EntryPoint.cs
+++ src/Google.Cloud.Functions.Invoker/EntryPoint.cs
@@ -12,11 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
