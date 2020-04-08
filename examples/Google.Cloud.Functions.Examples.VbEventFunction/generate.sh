e270bce1a200bc1fcad4e1c55c27d8b446d0b063
Jon Skeet <jonskeet@google.com>
2020-04-08 21:09:20 +0100

    Add VB projects to the examples solution

---
+++ examples/Google.Cloud.Functions.Examples.VbEventFunction/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+rm *.vb *.vbproj
+rm -rf bin obj
+
+# Create the project and source files
+dotnet new gcf-event -lang vb
+
+# Change the package reference to a project reference
+dotnet remove package Google.Cloud.Functions.Invoker
+dotnet add reference ../../src/Google.Cloud.Functions.Invoker
+
+# Explicitly import the MSBuild targets (not necessary for NuGet packages)
+sed -i '2 i \ \ <Import Project="../../src/Google.Cloud.Functions.Invoker/targets/Google.Cloud.Functions.Invoker.targets"/>' *.vbproj
+sed -i '3 i \ \ <Import Project="../../src/Google.Cloud.Functions.Invoker/targets/Google.Cloud.Functions.Invoker.props"/>' *.vbproj
