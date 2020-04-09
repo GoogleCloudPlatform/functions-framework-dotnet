4d440083c89bbc636e730b9ba2b1e9ad0a023e80
Jon Skeet <jonskeet@google.com>
2020-04-09 14:59:09 +0100

    Remove copyright headers from templates
    This matches what we do in Cloud Code.
    
    Fixes #34

--- src/Google.Cloud.Functions.Templates/templates/http-function-vb/CloudFunction.vb
+++ src/Google.Cloud.Functions.Templates/templates/http-function-vb/CloudFunction.vb
@@ -1,17 +1,3 @@
-' Copyright 2020, Google LLC
-'
-' Licensed under the Apache License, Version 2.0 (the "License");
-' you may Not use this file except In compliance With the License.
-' You may obtain a copy of the License at
-'
-'     https://www.apache.org/licenses/LICENSE-2.0
-'
-' Unless required by applicable law Or agreed to in writing, software
-' distributed under the License Is distributed on an "AS IS" BASIS,
-' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
-' See the License for the specific language governing permissions And
-' limitations under the License.
-
 Imports Google.Cloud.Functions.Framework
 Imports Microsoft.AspNetCore.Http
 
