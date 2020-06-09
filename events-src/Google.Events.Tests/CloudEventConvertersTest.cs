7697c71b456d540d4c1778b0f9f65497314f2512
Jon Skeet <jonskeet@google.com>
2020-06-09 16:07:03 +0100

    Initial code for Google.Events package
    This is intended to be a very lightweight package with no dependencies other than the CloudEvents SDK. The Functions Framework will then depend on this for data conversions, but not on any particular serialization package. Other packages (e.g. Google.Events.Protobuf) will contain serialization-type-specific event representations.

---
+++ events-src/Google.Events.Tests/CloudEventConvertersTest.cs
@@ -0,0 +1,129 @@
+﻿// Copyright 2020, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CloudNative.CloudEvents;
+using System;
+using System.Net.Mime;
+using Xunit;
+
+namespace Google.Events.Tests
+{
+    public class CloudEventConvertersTest
+    {
+        [Fact]
+        public void ConvertEventData_Valid()
+        {
+            var evt = new CloudEvent("type", new Uri("//source")) { Data = "some text" };
+            var data = CloudEventConverters.ConvertCloudEventData<EventData>(evt);
+            Assert.Equal("some text", data.Text);
+        }
+
+        [Fact]
+        public void PopulateCloudEvent_Valid()
+        {
+            var evt = new CloudEvent("type", new Uri("//source"));
+            var data = new EventData { Text = "some text" };
+            CloudEventConverters.PopulateCloudEvent(evt, data);
+            Assert.Equal("some text", evt.Data);
+        }
+
+        [Fact]
+        public void GetDataConverter_Cached()
+        {
+            var converter1 = CloudEventConverters.GetDataConverter<EventData>();
+            var converter2 = CloudEventConverters.GetDataConverter<EventData>();
+            Assert.Same(converter1, converter2);
+        }
+
+        [Fact]
+        public void GetDataConverter_NoCtor() =>
+            Assert.Throws<InvalidOperationException>(() => CloudEventConverters.GetDataConverter<NoCtorData>());
+
+        [Fact]
+        public void GetDataConverter_DoesntImplementInterface() =>
+            Assert.Throws<InvalidOperationException>(() => CloudEventConverters.GetDataConverter<NoImplementationData>());
+
+        [Fact]
+        public void GetDataConverter_WrongTargetType() =>
+            Assert.Throws<InvalidOperationException>(() => CloudEventConverters.GetDataConverter<WrongTargetTypeData>());
+
+        [Fact]
+        public void GetDataConverter_NoAttribute() =>
+            Assert.Throws<InvalidOperationException>(() => CloudEventConverters.GetDataConverter<NoAttributeData>());
+
+        [CloudEventDataConverter(typeof(EventDataConverter))]
+        private class EventData
+        {
+            public string Text { get; set; }
+        }
+
+        private class EventDataConverter : ICloudEventDataConverter<EventData>
+        {
+            public EventData ConvertEventData(CloudEvent cloudEvent) =>
+                new EventData { Text = (string) cloudEvent.Data };
+
+            public void PopulateCloudEvent(CloudEvent cloudEvent, EventData data)
+            {
+                cloudEvent.DataContentType = new ContentType("text/plain");
+                cloudEvent.Data = data.Text;
+            }
+        }
+
+        [CloudEventDataConverter(typeof(NoCtorConverter))]
+        private class NoCtorData
+        {
+        }
+
+        private class NoCtorConverter : ICloudEventDataConverter<NoCtorData>
+        {
+            // This constructor won't be found.
+            private NoCtorConverter()
+            {
+            }
+
+            public NoCtorData ConvertEventData(CloudEvent cloudEvent) => null;
+
+            public void PopulateCloudEvent(CloudEvent cloudEvent, NoCtorData data)
+            {
+            }
+        }
+
+        [CloudEventDataConverter(typeof(NoImplementationConverter))]
+        private class NoImplementationData
+        {
+        }
+
+        private class NoImplementationConverter
+        {
+        }
+
+        [CloudEventDataConverter(typeof(WrongTargetTypeConverter))]
+        private class WrongTargetTypeData
+        {
+        }
+
+        private class WrongTargetTypeConverter : ICloudEventDataConverter<string>
+        {
+            public string ConvertEventData(CloudEvent cloudEvent) => null;
+
+            public void PopulateCloudEvent(CloudEvent cloudEvent, string data)
+            {
+            }
+        }
+
+        private class NoAttributeData
+        {
+        }
+    }
+}
---
+++ events-src/Google.Events/CloudEventConverters.cs
@@ -0,0 +1,100 @@
+﻿// Copyright 2020, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CloudNative.CloudEvents;
+using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+namespace Google.Events
+{
+    /// <summary>
+    /// Utility class to obtain and execute converters by reflecting over attributes applied to
+    /// the target types, caching converters appropriately.
+    /// </summary>
+    public static class CloudEventConverters
+    {
+        private static readonly ConcurrentDictionary<Type, object> s_dataConverters =
+            new ConcurrentDictionary<Type, object>();
+
+        // Internal for testing. (We may want to make it public at some point, in which case we'd probably want to
+        // think about the name first.)
+        internal static ICloudEventDataConverter<T> GetDataConverter<T>() where T : class =>
+            (ICloudEventDataConverter<T>) s_dataConverters.GetOrAdd(typeof(T), type => CreateDataConverter<T>());
+
+        private static ICloudEventDataConverter<T> CreateDataConverter<T>() where T : class
+        {
+            try
+            {
+                var targetType = typeof(T);
+                var attribute = targetType.GetCustomAttribute<CloudEventDataConverterAttribute>(true);
+                // It's fine for the converter creation to fail: we'll try it on every attempt,
+                // and always end up with an exception.
+                if (attribute is null)
+                {
+                    throw new ArgumentException($"Type {targetType} does not have {nameof(CloudEventDataConverterAttribute)} applied to it.");
+                }
+                var converterType = attribute.ConverterType;
+                if (converterType is null)
+                {
+                    throw new ArgumentException($"The {nameof(CloudEventDataConverterAttribute)} on type {targetType} has no converter type specified.");
+                }
+
+                var instance = Activator.CreateInstance(converterType);
+
+                var converter = instance as ICloudEventDataConverter<T>;
+                if (converter is null)
+                {
+                    throw new ArgumentException($"Data converter type {converterType} does not implement {nameof(ICloudEventDataConverter<T>)} with a target type of {targetType}");
+                }
+
+                return converter;
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException($"Unable to create data converter for target type {typeof(T)}", e);
+            }
+        }
+
+        /// <summary>
+        /// Converts the data in <paramref name="cloudEvent"/> into the type specified by <typeparamref name="T"/>,
+        /// obtaining the <see cref="ICloudEventDataConverter{T}"/> associated with the type via <see cref="CloudEventDataConverterAttribute"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to convert the event data to.</typeparam>
+        /// <param name="cloudEvent">The CloudEvent to extract the data from. Must not be null.</param>
+        /// <returns>The converted data.</returns>
+        public static T ConvertCloudEventData<T>(CloudEvent cloudEvent) where T : class
+        {
+            EventsPreconditions.CheckNotNull(cloudEvent, nameof(cloudEvent));
+            var converter = GetDataConverter<T>();
+            return converter.ConvertEventData(cloudEvent);
+        }
+
+
+        /// <summary>
+        /// Populates <paramref name="cloudEvent"/> with the data in <paramref name="data"/> by
+        /// using the <see cref="ICloudEventDataConverter{T}"/> associated with <typeparamref name="T"/> via
+        /// <see cref="CloudEventDataConverterAttribute"/>. See
+        /// <see cref="ICloudEventDataConverter{T}.PopulateCloudEvent(CloudEvent, T)"/> for details what gets populated.
+        /// </summary>
+        /// <param name="cloudEvent"></param>
+        /// <param name="data"></param>
+        public static void PopulateCloudEvent<T>(CloudEvent cloudEvent, T data) where T : class
+        {
+            EventsPreconditions.CheckNotNull(cloudEvent, nameof(cloudEvent));
+            var converter = GetDataConverter<T>();
+            converter.PopulateCloudEvent(cloudEvent, data);
+        }
+    }
+}
