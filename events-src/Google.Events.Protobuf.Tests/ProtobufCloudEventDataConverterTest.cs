a33d52afd933fef938f1e03dcd9ed3426ae094b7
Jon Skeet <jonskeet@google.com>
2020-06-10 12:05:23 +0100

    ProtobufCloudEventDataConverter, attributes and tests

---
+++ events-src/Google.Events.Protobuf.Tests/ProtobufCloudEventDataConverterTest.cs
@@ -0,0 +1,99 @@
+﻿// Copyright 2020, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CloudNative.CloudEvents;
+using Google.Events.Protobuf.Cloud.Storage.V1;
+using Google.Protobuf;
+using System;
+using System.Net.Mime;
+using Xunit;
+
+namespace Google.Events.Protobuf.Tests
+{
+    public class ProtobufCloudEventDataConverterTest
+    {
+        private static readonly ContentType JsonContentType = new ContentType("application/json");
+        private static readonly ContentType ProtobufContentType = new ContentType("application/protobuf");
+
+        [Fact]
+        public void ConvertEventData_BadDataType()
+        {
+            var evt = new CloudEvent("type", new Uri("//source")) { Data = Guid.NewGuid() };
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            Assert.Throws<ArgumentException>(() => converter.ConvertEventData(evt));
+        }
+
+        [Fact]
+        public void ConvertEventData_Binary()
+        {
+            var original = new StorageObject { Bucket = "my-bucket" };
+            var evt = new CloudEvent("type", new Uri("//source")) { Data = original.ToByteArray() };
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            var result = converter.ConvertEventData(evt);
+            Assert.Equal(original, result);
+        }
+
+        [Fact]
+        public void ConvertEventData_Json()
+        {
+            var original = new StorageObject { Bucket = "my-bucket" };
+            var evt = new CloudEvent("type", new Uri("//source")) { Data = "{ \"bucket\": \"my-bucket\" }" };
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            var result = converter.ConvertEventData(evt);
+            Assert.Equal(original, result);
+        }
+
+        [Fact]
+        public void PopulateCloudEvent_DefaultToJson()
+        {
+            var data = new StorageObject { Bucket = "my-bucket" };
+            var evt = new CloudEvent("type", new Uri("//source"));
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            converter.PopulateCloudEvent(evt, data);
+            Assert.Equal("{ \"bucket\": \"my-bucket\" }", evt.Data);
+            Assert.Equal(JsonContentType, evt.DataContentType);
+        }
+
+        [Fact]
+        public void PopulateCloudEvent_ExplicitlyJson()
+        {
+            var data = new StorageObject { Bucket = "my-bucket" };
+            var evt = new CloudEvent("type", new Uri("//source")) { DataContentType = JsonContentType };
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            converter.PopulateCloudEvent(evt, data);
+            Assert.Equal("{ \"bucket\": \"my-bucket\" }", evt.Data);
+            Assert.Equal(JsonContentType, evt.DataContentType);
+        }
+
+        [Fact]
+        public void PopulateCloudEvent_ExplicitlyBinary()
+        {
+            var data = new StorageObject { Bucket = "my-bucket" };
+            var evt = new CloudEvent("type", new Uri("//source")) { DataContentType = ProtobufContentType };
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            converter.PopulateCloudEvent(evt, data);
+            Assert.Equal(data.ToByteArray(), evt.Data);
+            Assert.Equal(ProtobufContentType, evt.DataContentType);
+        }
+
+        [Fact]
+        public void PopulateCloudEvent_UnknownDataContentType()
+        {
+            var data = new StorageObject { Bucket = "my-bucket" };
+            var evt = new CloudEvent("type", new Uri("//source")) { DataContentType = new ContentType("application/unknown") };
+            var converter = new ProtobufCloudEventDataConverter<StorageObject>();
+            Assert.Throws<ArgumentException>(() => converter.PopulateCloudEvent(evt, data));
+        }
+    }
+}
---
+++ events-src/Google.Events.Protobuf/Cloud/Firestore/V1/Document.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2020, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Protobuf.Collections;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+// This file contains manual additions to the code generated from the protos.
+
+namespace Google.Events.Protobuf.Cloud.Firestore.V1
+{
+    public partial class Document
+    {
+        /// <summary>
+        /// Converts the fields in <see cref="Fields"/> into an idiomatic .NET representation.
+        /// </summary>
+        public IReadOnlyDictionary<string, object> ConvertFields() => ConvertMap(Fields);
+
+        private static IReadOnlyDictionary<string, object> ConvertMap(MapField<string, Value> fields)
+        {
+            var dictionary = fields.ToDictionary(pair => pair.Key, pair => ConvertValue(pair.Value));
+            return new ReadOnlyDictionary<string, object>(dictionary);
+        }
+
+        private static object ConvertValue(Value value) => value.ValueTypeCase switch
+        {
+            Value.ValueTypeOneofCase.ArrayValue => value.ArrayValue.Values.Select(ConvertValue).ToList().AsReadOnly(),
+            Value.ValueTypeOneofCase.BooleanValue => value.BooleanValue,
+            Value.ValueTypeOneofCase.DoubleValue => value.DoubleValue,
+            Value.ValueTypeOneofCase.GeoPointValue => value.GeoPointValue,
+            Value.ValueTypeOneofCase.IntegerValue => value.IntegerValue,
+            Value.ValueTypeOneofCase.MapValue => ConvertMap(value.MapValue.Fields),
+            Value.ValueTypeOneofCase.NullValue => null,
+            Value.ValueTypeOneofCase.ReferenceValue => value.ReferenceValue,
+            Value.ValueTypeOneofCase.StringValue => value.StringValue,
+            Value.ValueTypeOneofCase.TimestampValue => value.TimestampValue.ToDateTimeOffset(),
+            Value.ValueTypeOneofCase.BytesValue => value.BytesValue.ToByteArray(),
+            // FIXME: This shouldn't be necessary, but we don't parse (or format) the JSON value of NullValue correctly at the moment.
+            Value.ValueTypeOneofCase.None => null,
+            _ => throw new ArgumentException($"Invalid value case: {value.ValueTypeCase}")
+        };
+    }
+}
