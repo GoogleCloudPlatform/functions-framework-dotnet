b9c6d39f196f96d39f59d7a7f0a7cc955fafc2b7
Jon Skeet <jonskeet@google.com>
2020-06-26 08:47:18 +0100

    Refactor event conversion to use inheritance rather than special-casing
    One test has been deleted as we can always provide a service name if the event doesn't contain it, based on the event type.

--- src/Google.Cloud.Functions.Framework/GcfEvents/GcfConverters.cs
+++ src/Google.Cloud.Functions.Framework/GcfEvents/GcfConverters.cs
@@ -32,52 +32,37 @@ namespace Google.Cloud.Functions.Framework.GcfEvents
         private static readonly string JsonContentTypeText = "application/json";
         private static readonly ContentType JsonContentType = new ContentType(JsonContentTypeText);
 
-        // Regex to split a storage resource name into bucket and object names.
-        // Note the removal of "#" followed by trailing digits at the end of the object name;
-        // this represents the object generation and should not be included in the CloudEvent subject.
-        private static readonly Regex StorageResourcePattern =
-            new Regex(@"^(projects/_/buckets/[^/]+)/(objects/.*?)(?:#\d+)?$", RegexOptions.Compiled);
-
-        private static readonly Dictionary<string, string> s_eventTypeMapping = new Dictionary<string, string>
+        private static readonly Dictionary<string, EventAdapter> s_eventTypeMapping = new Dictionary<string, EventAdapter>
         {
-            { "google.pubsub.topic.publish", "com.google.cloud.pubsub.topic.publish.v0" },
-            { "google.storage.object.finalize", "com.google.cloud.storage.object.finalize.v0" },
-            { "google.storage.object.delete", "com.google.cloud.storage.object.delete.v0" },
-            { "google.storage.object.archive", "com.google.cloud.storage.object.archive.v0" },
-            { "google.storage.object.metadataUpdate", "com.google.cloud.storage.object.metadataUpdate.v0" },
-            { "providers/cloud.firestore/eventTypes/document.write", "com.google.cloud.firestore.document.write.v0" },
-            { "providers/cloud.firestore/eventTypes/document.create", "com.google.cloud.firestore.document.create.v0" },
-            { "providers/cloud.firestore/eventTypes/document.update", "com.google.cloud.firestore.document.update.v0" },
-            { "providers/cloud.firestore/eventTypes/document.delete", "com.google.cloud.firestore.document.delete.v0" },
-            { "providers/firebase.auth/eventTypes/user.create", "com.google.cloud.firebase.auth.user.create.v0" },
-            { "providers/firebase.auth/eventTypes/user.delete", "com.google.cloud.firebase.auth.user.delete.v0" },
-            { "providers/google.firebase.analytics/eventTypes/event.log", "com.google.cloud.firebase.analytics.log.v0" },
-            { "providers/google.firebase.database/eventTypes/ref.create", "com.google.cloud.firebase.database.create.v0" },
-            { "providers/google.firebase.database/eventTypes/ref.write", "com.google.cloud.firebase.database.write.v0" },
-            { "providers/google.firebase.database/eventTypes/ref.update", "com.google.cloud.firebase.database.update.v0" },
-            { "providers/google.firebase.database/eventTypes/ref.delete", "com.google.cloud.firebase.database.delete.v0" },
-            { "providers/cloud.pubsub/eventTypes/topic.publish", "com.google.cloud.pubsub.topic.publish.v0" },
-            { "providers/cloud.storage/eventTypes/object.change", "com.google.cloud.storage.object.change.v0" },
-        };
-
-        /// <summary>
-        /// Mappings from GCF event types to services, for event types which don't include the service in the context.
-        /// </summary>
-        private static readonly Dictionary<string, string> s_serviceMapping = new Dictionary<string, string>
-        {
-            { "providers/cloud.firestore/eventTypes/document.write", "firestore.googleapis.com" },
-            { "providers/cloud.firestore/eventTypes/document.create", "firestore.googleapis.com" },
-            { "providers/cloud.firestore/eventTypes/document.update", "firestore.googleapis.com" },
-            { "providers/cloud.firestore/eventTypes/document.delete", "firestore.googleapis.com" },
-            { "providers/google.firebase.analytics/eventTypes/event.log", "firebase.googleapis.com" },
-            { "providers/firebase.auth/eventTypes/user.create", "firebase.googleapis.com" },
-            { "providers/firebase.auth/eventTypes/user.delete", "firebase.googleapis.com" },
-            { "providers/google.firebase.database/eventTypes/ref.create", "firebase.googleapis.com" },
-            { "providers/google.firebase.database/eventTypes/ref.write", "firebase.googleapis.com" },
-            { "providers/google.firebase.database/eventTypes/ref.update", "firebase.googleapis.com" },
-            { "providers/google.firebase.database/eventTypes/ref.delete", "firebase.googleapis.com" },
-            { "providers/cloud.pubsub/eventTypes/topic.publish", "pubsub.googleapis.com" },
-            { "providers/cloud.storage/eventTypes/object.change", "storage.googleapis.com" },
+            { "google.pubsub.topic.publish", new PubSubEventAdapter("com.google.cloud.pubsub.topic.publish.v0") },
+            { "google.storage.object.finalize", new StorageEventAdapter("com.google.cloud.storage.object.finalize.v0") },
+            { "google.storage.object.delete", new StorageEventAdapter("com.google.cloud.storage.object.delete.v0") },
+            { "google.storage.object.archive", new StorageEventAdapter("com.google.cloud.storage.object.archive.v0") },
+            { "google.storage.object.metadataUpdate", new StorageEventAdapter("com.google.cloud.storage.object.metadataUpdate.v0") },
+            { "providers/cloud.firestore/eventTypes/document.write",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firestore.document.write.v0", "firestore.googleapis.com") },
+            { "providers/cloud.firestore/eventTypes/document.create",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firestore.document.create.v0", "firestore.googleapis.com") },
+            { "providers/cloud.firestore/eventTypes/document.update",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firestore.document.update.v0", "firestore.googleapis.com") },
+            { "providers/cloud.firestore/eventTypes/document.delete",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firestore.document.delete.v0", "firestore.googleapis.com") },
+            { "providers/firebase.auth/eventTypes/user.create",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.auth.user.create.v0", "firebase.googleapis.com") },
+            { "providers/firebase.auth/eventTypes/user.delete",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.auth.user.delete.v0", "firebase.googleapis.com") },
+            { "providers/google.firebase.analytics/eventTypes/event.log",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.analytics.log.v0", "firebase.googleapis.com") },
+            { "providers/google.firebase.database/eventTypes/ref.create",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.database.create.v0", "firebase.googleapis.com") },
+            { "providers/google.firebase.database/eventTypes/ref.write",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.database.write.v0", "firebase.googleapis.com") },
+            { "providers/google.firebase.database/eventTypes/ref.update",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.database.update.v0", "firebase.googleapis.com") },
+            { "providers/google.firebase.database/eventTypes/ref.delete",
+                new FirestoreFirebaseEventAdapter("com.google.cloud.firebase.database.delete.v0", "firebase.googleapis.com") },
+            { "providers/cloud.pubsub/eventTypes/topic.publish", new PubSubEventAdapter("com.google.cloud.pubsub.topic.publish.v0") },
+            { "providers/cloud.storage/eventTypes/object.change", new StorageEventAdapter("com.google.cloud.storage.object.change.v0") },
         };
 
         internal static async ValueTask<CloudEvent> ConvertGcfEventToCloudEvent(HttpRequest request)
@@ -85,54 +70,11 @@ namespace Google.Cloud.Functions.Framework.GcfEvents
             var jsonRequest = await ParseRequest(request);
             // Validated as not null or empty in ParseRequest
             string gcfType = jsonRequest.Context.Type!;
-            if (!s_eventTypeMapping.TryGetValue(gcfType, out var cloudEventType))
+            if (!s_eventTypeMapping.TryGetValue(gcfType, out var eventAdapter))
             {
                 throw new CloudEventConverter.ConversionException($"Unexpected event type for function: '{gcfType}'");
             }
-
-            // Firestore and Firebase database GCF requests include a "params" element that maps wilcards in the resource
-            // template to their value in the changed document.
-            if (gcfType.StartsWith("providers/cloud.firestore/", StringComparison.Ordinal) ||
-                gcfType.StartsWith("providers/google.firebase.database/", StringComparison.Ordinal))
-            {
-                if (jsonRequest.Params is object)
-                {
-                    jsonRequest.Data["wildcards"] = jsonRequest.Params;
-                }
-            }
-
-            if (cloudEventType == "com.google.cloud.pubsub.topic.publish.v0")
-            {
-                // Wrap the PubSub message in a wrapper for consistency with Cloud Run.
-                jsonRequest.Data = new Dictionary<string, object> { { "message", jsonRequest.Data } };
-            }
-
-            var context = jsonRequest.Context;
-            var resource = context.Resource;
-            var service = ValidateNotNullOrEmpty(resource.Service ?? s_serviceMapping.GetValueOrDefault(gcfType), "service");
-            var name = ValidateNotNullOrEmpty(resource.Name, "resource name");
-            string? subject = null;
-            if (service == "storage.googleapis.com" && StorageResourcePattern.Match(name) is { Success: true } match)
-            {
-                // Resource name up to the bucket
-                name = match.Groups[1].Value;
-                // Resource name from "objects" onwards, but without the generation
-                subject = match.Groups[2].Value;
-            }
-            var source = new Uri($"//{service}/{name}");
-
-            var evt = new CloudEvent(cloudEventType, source, context.Id, context.Timestamp?.UtcDateTime)
-            {
-                Data = JsonSerializer.Serialize(jsonRequest.Data),
-                DataContentType = JsonContentType
-            };
-            // CloudEvent.Subject is null by default, but doesn't allow null to be set.
-            // See https://github.com/cloudevents/sdk-csharp/issues/58
-            if (subject is string)
-            {
-                evt.Subject = subject;
-            }
-            return evt;
+            return eventAdapter.ConvertToCloudEvent(jsonRequest);
         }
 
         private static async ValueTask<Request> ParseRequest(HttpRequest request)
@@ -168,5 +110,108 @@ namespace Google.Cloud.Functions.Framework.GcfEvents
             string.IsNullOrEmpty(value)
             ? throw new CloudEventConverter.ConversionException($"Event contained no {name}")
             : value;
+
+        /// <summary>
+        /// An event-specific adapter, picked based on the event type from the GCF event.
+        /// This abstract class provides seams for event-specific changes to the data and the source/subject.
+        /// </summary>
+        private abstract class EventAdapter
+        {
+            private readonly string _cloudEventType;
+            private readonly string _defaultService;
+
+            protected EventAdapter(string cloudEventType, string defaultService) =>
+                (_cloudEventType, _defaultService) = (cloudEventType, defaultService);
+
+            internal CloudEvent ConvertToCloudEvent(Request request)
+            {
+                MaybeReshapeData(request);
+
+                var context = request.Context;
+                var resource = context.Resource;
+                var service = ValidateNotNullOrEmpty(resource.Service ?? _defaultService, "service");
+                var resourceName = ValidateNotNullOrEmpty(resource.Name, "resource name");
+                var (source, subject) = ConvertResourceToSourceAndSubject(service, resourceName);
+                var evt = new CloudEvent(_cloudEventType, source, context.Id, context.Timestamp?.UtcDateTime)
+                {
+                    Data = JsonSerializer.Serialize(request.Data),
+                    DataContentType = JsonContentType
+                };
+                // CloudEvent.Subject is null by default, but doesn't allow null to be set.
+                // See https://github.com/cloudevents/sdk-csharp/issues/58
+                if (subject is string)
+                {
+                    evt.Subject = subject;
+                }
+                return evt;
+            }
+
+            protected virtual void MaybeReshapeData(Request request) { }
+            protected virtual (Uri source, string? subject) ConvertResourceToSourceAndSubject(string service, string resource) =>
+                (new Uri($"//{service}/{resource}"), null);
+        }
+
+        private class StorageEventAdapter : EventAdapter
+        {
+            // Regex to split a storage resource name into bucket and object names.
+            // Note the removal of "#" followed by trailing digits at the end of the object name;
+            // this represents the object generation and should not be included in the CloudEvent subject.
+            private static readonly Regex StorageResourcePattern =
+                new Regex(@"^(projects/_/buckets/[^/]+)/(objects/.*?)(?:#\d+)?$", RegexOptions.Compiled);
+
+            internal StorageEventAdapter(string cloudEventType) : base(cloudEventType, "storage.googleapis.com")
+            {
+            }
+
+            /// <summary>
+            /// Split a Storage object resource name into bucket (in the source) and object (in the subject)
+            /// </summary>
+            protected override (Uri source, string? subject) ConvertResourceToSourceAndSubject(string service, string resource)
+            {
+                if (StorageResourcePattern.Match(resource) is { Success: true } match)
+                {
+                    // Resource name up to the bucket
+                    resource = match.Groups[1].Value;
+                    // Resource name from "objects" onwards, but without the generation
+                    var subject = match.Groups[2].Value;
+                    return (new Uri($"//{service}/{resource}"), subject);
+                }
+                // If the resource name didn't match for whatever reason, just use the default behaviour.
+                return base.ConvertResourceToSourceAndSubject(service, resource);
+            }
+        }
+
+        private class PubSubEventAdapter : EventAdapter
+        {
+            internal PubSubEventAdapter(string cloudEventType) : base(cloudEventType, "pubsub.googleapis.com")
+            {
+            }
+
+            /// <summary>
+            /// Wrap the PubSub message for consistency with Cloud Run.
+            /// </summary>
+            protected override void MaybeReshapeData(Request request) =>
+                request.Data = new Dictionary<string, object> { { "message", request.Data } };
+        }
+
+        private class FirestoreFirebaseEventAdapter : EventAdapter
+        {
+            internal FirestoreFirebaseEventAdapter(string cloudEventType, string defaultService)
+                : base(cloudEventType, defaultService)
+            {
+            }
+
+            /// <summary>
+            /// Firestore and Firebase database GCF requests include a "params" element that maps wildcards in the resource
+            /// template to their value in the changed document. We map this to a "wildcards" property in the data.
+            /// </summary>
+            protected override void MaybeReshapeData(Request request)
+            {
+                if (request.Params is object)
+                {
+                    request.Data["wildcards"] = request.Params;
+                }
+            }
+        }
     }
 }
