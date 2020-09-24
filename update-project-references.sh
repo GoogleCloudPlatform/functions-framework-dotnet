24a8300163c82536cba242762537b278eb22deaf
Jon Skeet <jonskeet@google.com>
2020-09-24 16:51:52 +0100

    Update tooling references to Invoker

--- update-project-references.sh
+++ update-project-references.sh
@@ -6,11 +6,11 @@ VERSION=$(grep '<Version>.*</Version>' src/CommonProperties.xml  | sed -e 's/<\/
 
 for proj in $(find src/Google.Cloud.Functions.Templates/templates -name '*proj')
 do
-  sed -i -e "s/Include=\"Google.Cloud.Functions.Invoker\" Version=\".*\"/Include=\"Google.Cloud.Functions.Invoker\" Version=\"$VERSION\"/g" $proj
+  sed -i -e "s/Include=\"Google.Cloud.Functions.Hosting\" Version=\".*\"/Include=\"Google.Cloud.Functions.Hosting\" Version=\"$VERSION\"/g" $proj
 done
 
 for proj in $(find examples -name '*proj')
 do
-  sed -i -e "s/Include=\"Google.Cloud.Functions.Invoker\" Version=\".*\"/Include=\"Google.Cloud.Functions.Invoker\" Version=\"$VERSION\"/g" $proj
-  sed -i -e "s/Include=\"Google.Cloud.Functions.Invoker.Testing\" Version=\".*\"/Include=\"Google.Cloud.Functions.Invoker.Testing\" Version=\"$VERSION\"/g" $proj
+  sed -i -e "s/Include=\"Google.Cloud.Functions.Hosting\" Version=\".*\"/Include=\"Google.Cloud.Functions.Hosting\" Version=\"$VERSION\"/g" $proj
+  sed -i -e "s/Include=\"Google.Cloud.Functions.Testing\" Version=\".*\"/Include=\"Google.Cloud.Functions.Testing\" Version=\"$VERSION\"/g" $proj
 done
