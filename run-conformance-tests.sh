bf8ed784ab3f645facf2ef7c55bdf55f46ea2fa0
Jon Skeet <jonskeet@google.com>
2020-09-25 07:25:31 +0100

    chore: Update conformance test script
    - Exits with useful error message if the submodule is not present
    - Detects binary to use based on operating system
    - Passes buildpacks=false (needed for recent test runner changes)
    - Use the built DLL directly instead of "dotnet run" for improved portablility

--- run-conformance-tests.sh
+++ run-conformance-tests.sh
@@ -6,13 +6,20 @@
 # That repository is included as a submodule already, under
 # functions-framework-conformance
 
-# This script assumes the submodule is already up-to-date, but not
-# necessarily built. It assumes that "go" is already in the path.
+# This script checks that the submodule is already present, but
+# does not assume it has already been built.
+# It assumes that "go" is already in the path.
 
 set -e
 
 rm -rf tmp/conformance-test-output
 mkdir -p tmp/conformance-test-output
+
+if [[ ! -f functions-framework-conformance/README.md ]]
+then
+  echo "Conformance test repo not found. Init and update submodules."
+  exit 1
+fi
 
 # Build the conformance test framework
 echo "Building conformance test framework"
@@ -22,6 +29,31 @@ echo "Building conformance test framework"
 echo "Building conformance functions"
 dotnet build -nologo -clp:NoSummary -v quiet -c Release src/Google.Cloud.Functions.ConformanceTests
 
+CLIENT_BINARY=functions-framework-conformance/client/client
+if [[ $OSTYPE =~ ^win* || $OSTYPE =~ ^msys* || $OSTYPE =~ ^cygwin* ]]
+then
+  CLIENT_BINARY=${CLIENT_BINARY}.exe
+fi
+
+# Run the Functions Framework once as a "warm-up", killing it after 5 seconds.
+# This is necessary on MacOS for non-obvious reasons;
+# it's possible that the conformance test runner just expects it to be
+# ready a little bit earlier than it is.
+# TODO: Remove this when we can.
+echo "Running Functions Framework for 5 seconds as a warm-up step."
+dotnet src/Google.Cloud.Functions.ConformanceTests/bin/Release/netcoreapp3.1/Google.Cloud.Functions.ConformanceTests.dll HttpFunction &
+DOTNETPID=$!
+sleep 5
+kill $DOTNETPID
+
+# Note: we run the DLL directly rather than using "dotnet run" as this
+# responds more correctly to being killed by the conformance test runner
+# on Linux.
+DOTNET_DLL=src/Google.Cloud.Functions.ConformanceTests/bin/Release/netcoreapp3.1/Google.Cloud.Functions.ConformanceTests.dll
+
+echo "Using conformance test runner binary: $CLIENT_BINARY"
+echo "Using Functions Framework test binary: $DOTNET_DLL"
+
 # Run the tests
 run_test() {
   TEST_TYPE=$1
@@ -30,13 +62,14 @@ run_test() {
   (cd tmp/conformance-test-output && \
    mkdir $TEST_TYPE && \
    cd $TEST_TYPE && \
-   ../../../functions-framework-conformance/client/client.exe \
-     -type $TEST_TYPE \
-     -cmd "dotnet run --no-build -p ../../../src/Google.Cloud.Functions.ConformanceTests -c Release -- $TEST_FUNCTION" \
+   ../../../$CLIENT_BINARY \
+     -buildpacks=false \
+     -type=$TEST_TYPE \
+     -cmd="dotnet ../../../$DOTNET_DLL $TEST_FUNCTION" \
   )
 }
 
 run_test http HttpFunction
 run_test cloudevent UntypedCloudEventFunction
 
-echo "Tests complete"
+echo "Tests completed successfully"
