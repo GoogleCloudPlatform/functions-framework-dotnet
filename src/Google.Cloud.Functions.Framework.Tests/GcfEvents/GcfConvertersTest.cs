5770a19133e189124240c2a527a477837c1ca448
Jon Skeet <jonskeet@google.com>
2020-11-16 16:51:57 +0000

    feat: Support remote config update event conversion

--- src/Google.Cloud.Functions.Framework.Tests/GcfEvents/GcfConvertersTest.cs
+++ src/Google.Cloud.Functions.Framework.Tests/GcfEvents/GcfConvertersTest.cs
@@ -35,6 +35,7 @@ namespace Google.Cloud.Functions.Framework.Tests.GcfEvents
         [InlineData("firebase-db1.json", "google.firebase.database.document.v1.written", "//firebase.googleapis.com/projects/_/instances/my-project-id", "refs/gcf-test/xyz")]
         [InlineData("firebase-auth1.json", "google.firebase.auth.user.v1.created", "//firebase.googleapis.com/projects/my-project-id", null)]
         [InlineData("firebase-auth2.json", "google.firebase.auth.user.v1.deleted", "//firebase.googleapis.com/projects/my-project-id", null)]
+        [InlineData("firebase-remote-config.json", "google.firebase.remoteconfig.remoteConfig.v1.updated", "//firebaseremoteconfig.googleapis.com/projects/sample-project", null)]
         public async Task ConvertGcfEvent(string resourceName, string expectedType, string expectedSource, string expectedSubject)
         {
             var context = GcfEventResources.CreateHttpContext(resourceName);
