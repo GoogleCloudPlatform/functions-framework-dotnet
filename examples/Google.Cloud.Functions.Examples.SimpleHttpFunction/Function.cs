80d487296661fd3eff091f7abca4215242b181dc
Jon Skeet <jonskeet@google.com>
2022-10-25 11:28:25 +0100

    chore: Regenerate examples with new templates

--- examples/Google.Cloud.Functions.Examples.SimpleHttpFunction/Function.cs
+++ examples/Google.Cloud.Functions.Examples.SimpleHttpFunction/Function.cs
@@ -16,18 +16,17 @@ using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
-namespace Google.Cloud.Functions.Examples.SimpleHttpFunction
+namespace Google.Cloud.Functions.Examples.SimpleHttpFunction;
+
+public class Function : IHttpFunction
 {
-    public class Function : IHttpFunction
+    /// <summary>
+    /// Logic for your function goes here.
+    /// </summary>
+    /// <param name="context">The HTTP context, containing the request and the response.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task HandleAsync(HttpContext context)
     {
-        /// <summary>
-        /// Logic for your function goes here.
-        /// </summary>
-        /// <param name="context">The HTTP context, containing the request and the response.</param>
-        /// <returns>A task representing the asynchronous operation.</returns>
-        public async Task HandleAsync(HttpContext context)
-        {
-            await context.Response.WriteAsync("Hello, Functions Framework.");
-        }
+        await context.Response.WriteAsync("Hello, Functions Framework.");
     }
 }
