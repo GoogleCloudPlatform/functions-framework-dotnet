a54f4589458dd9d779afe1809d4c00eb23bf6943
Jon Skeet <jonskeet@google.com>
2023-09-01 14:00:04 +0100

    chore: Migrate from Python to .NET for release progress reporting

--- .kokoro/autorelease.sh
+++ .kokoro/autorelease.sh
@@ -11,33 +11,15 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 cd $SCRIPT_DIR/..
 
-# Make sure secrets are loaded in a well known location before running releasetool
+# Make sure secrets are loaded in a well known location before running
+# the release reporter script.
 source $SCRIPT_DIR/populatesecrets.sh
 populate_all_secrets
 
 NUGET_API_KEY="$(cat "$SECRETS_LOCATION"/google-cloud-nuget-api-key)"
 
-# Make sure we have the most recent version of pip, then install other packages.
-python -m pip install --require-hashes -r $SCRIPT_DIR/pip-requirements.txt
-python -m pip install --require-hashes -r $SCRIPT_DIR/requirements.txt
-python -m releasetool publish-reporter-script > /tmp/publisher-script
-
-# The publish reporter script uses "python3" which doesn't exist on Windows.
-# Work out what we should use instead.
-# Try to detect Python 3. It's quite different between Windows and Linux.
-if which python > /dev/null && python --version 2>&1 | grep -q "Python 3"; then declare -r PYTHON3=python
-elif which py > /dev/null && py -3 --version 2>&1 | grep -q "Python 3"; then declare -r PYTHON3="py -3"
-elif which python3 > /dev/null && python3 --version 2>&1 | grep -q "Python 3"; then declare -r PYTHON3=python3
-else
-  echo "Unable to detect Python 3 installation."
-  exit 1
-fi
-
-# Fix up the publish reporter script using $PYTHON3. We assume this won't
-# be harmful within sed - at the moment it's always "python", "py -3" or "python3".
-sed -i "s/python3/$PYTHON3/g" /tmp/publisher-script
-
-source /tmp/publisher-script
+dotnet tool restore
+source <(dotnet release-progress-reporter publish-reporter-script)
 
 COMMITTISH=$COMMITTISH_OVERRIDE
 if [[ $COMMITTISH_OVERRIDE = "" ]]
