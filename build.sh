fb0a8f88a660930d1d94a063c127365966af5bec
Amanda Tarafa Mas <14878252+amanda-tarafa@users.noreply.github.com>
2025-05-30 01:32:32 -0700

    ci: Bring Sourcelink configuration to internal scripts.
    See b/421332922

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-# Make sure that SourceLink uses the GitHub repo, even if that's not where
-# our origin remote points at.
-git remote add github https://github.com/GoogleCloudPlatform/functions-framework-dotnet.git
-export GitRepositoryRemoteName=github
-
 export ContinuousIntegrationBuild=true
 export Configuration=Release
 # When building examples, build against the version in this
@@ -28,7 +23,3 @@ dotnet pack -nologo -v quiet src -o $PWD/nupkg
 
 echo Created packages:
 ls nupkg
-
-# Remove the github remote so that if there are multiple iterations
-# against the same clone, the "git remote add" earlier will work.
-git remote remove github
